b23159b7086a43d08fda921a369a0d882bba046c
Navin Shrinivas <karupal2002@gmail.com>
2023-12-18 07:02:57 +0530

    moving install paths to /usr/local/

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo rm -rf /usr/local/src/sapling
 sudo mkdir -p /usr/local/src/sapling
 sudo cp -r ~/sapling/* /usr/local/src/sapling/
 cd /usr/local/src/sapling
-sudo rm -rf /usr/bin/sapling
-sudo ln -sf /usr/local/src/sapling/target/release/sapling /usr/bin/sapling 
-sudo chmod +x /usr/bin/sapling
+sudo mkdir -p /usr/local/bin/
+sudo rm -rf /usr/local/bin/sapling
+sudo ln -sf /usr/local/src/sapling/target/release/sapling /usr/local/bin/sapling 
+sudo chmod +x /usr/local/bin/sapling
